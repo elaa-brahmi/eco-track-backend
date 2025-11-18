1435a415ddf8e62bb2d9ee95d0ac845f5ae17015
elaa <elaa.brahmii@gmail.com>
2025-11-18 18:13:15 +0100

    add user class+ repository

---
+++ src/main/java/com/example/demo/models/User.java
@@ -0,0 +1,26 @@
+package com.example.demo.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.Instant;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document("employees")
+public class User {
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private Role roleUser;
+    private Instant createdAt;
+
+}
