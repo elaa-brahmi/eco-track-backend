de753e10d7c1c620ffd3c960aad00c9d2e05f755
elaa <elaa.brahmii@gmail.com>
2025-11-18 12:29:26 +0100

    complete test classes for all controllers

--- src/test/java/com/example/demo/controller/HealthControllerTest.java
+++ src/test/java/com/example/demo/controller/HealthControllerTest.java
@@ -1,12 +1,25 @@
 package com.example.demo.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@WebMvcTest(HealthController.class)
 class HealthControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void health() {
+    void health() throws Exception {
+        mockMvc.perform(get("/health"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.status").isString());
     }
 }
