5cf0e5a35bfd5c4307c71109961262bfb55de7c8
elaa <elaa.brahmii@gmail.com>
2025-11-19 21:07:51 +0100

    remove id from employee class

--- src/main/java/com/example/demo/models/Employee.java
+++ src/main/java/com/example/demo/models/Employee.java
@@ -2,18 +2,16 @@ package com.example.demo.models;
 
 
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@SuperBuilder
 @Document("employees")
 public class Employee extends User{
-
-    @Id
-    private String id;
     private String role;
     private boolean available;
 }
