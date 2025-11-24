239747cc03c50b3dbe2ceefa563225901f3f3be9
elaa <elaa.brahmii@gmail.com>
2025-11-24 18:26:31 +0100

    remove unused dependencies

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -9,6 +9,7 @@ import com.example.demo.repositories.EmployeeRepository;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
