4968d5ddee5af50783ac5e10d7741773abfd51b4
elaa <elaa.brahmii@gmail.com>
2025-11-24 23:32:01 +0100

    update delete employee to delete from keycloak and mongodb

--- src/main/java/com/example/demo/repositories/EmployeeRepository.java
+++ src/main/java/com/example/demo/repositories/EmployeeRepository.java
@@ -7,5 +7,6 @@ import java.util.Optional;
 public interface EmployeeRepository extends MongoRepository<Employee, String> {
     Optional<Employee> findByKeycloakId(String keycloakId);
     boolean existsByKeycloakId(String keycloakId);
+    void deleteByKeycloakId(String keycloakId);
 }
 
